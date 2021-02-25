ba8d5d26e184de213a7d6d9e81a2fff81c80b2bd
Cory LaViska <cory@abeautifulsite.net>
2021-02-25 08:33:11 -0500

    update for new build

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-// Default stylesheet
 import '@shoelace-style/shoelace/dist/shoelace.css';
-
-// For tree shaking to work, import components and utilities from their own module
-import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
-import SlButton from '@shoelace-style/shoelace/dist/components/button/button';
-import SlIcon from '@shoelace-style/shoelace/dist/components/icon/icon';
-import SlInput from '@shoelace-style/shoelace/dist/components/input/input';
-import SlRating from '@shoelace-style/shoelace/dist/components/rating/rating';
-//
-// ...and any other components you want
-//
+import { setBasePath, SlButton, SlIcon, SlInput, SlRating } from '@shoelace-style/shoelace';
 
 // Set the pase path to the folder you copied Shoelace's assets to
-setBasePath('/assets');
+setBasePath('/dist/shoelace');
+
+SlButton.register();
+SlIcon.register();
+SlInput.register();
+SlRating.register();
 
 // <sl-button>, <sl-icon>, <sl-input>, and <sl-rating> are ready to use!
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,20 +8,28 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  // Important! You must set this for treeshaking to work properly
+  // More info: https://webpack.js.org/configuration/optimization/#optimizationsideeffects
+  optimization: {
+    sideEffects: true,
+  },
   module: {
+    // Bundle styles into main.css
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new CopyPlugin({
       patterns: [
+        // Copy Shoelace assets to dist/shoelace
         {
           from: path.resolve(__dirname, 'node_modules/@shoelace-style/shoelace/dist/assets'),
-          to: path.resolve(__dirname, 'assets')
+          to: path.resolve(__dirname, 'dist/shoelace/assets')
         }
       ]
     })
